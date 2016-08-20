4f91c51bd11d8408e06cb09a5bf631328b41b5a7
ipp-docker-hub-client <ipp-docker-hub-client@users.noreply.github.com>
2016-08-21 01:00:19 +0800

    Update docker-entrypoint.sh

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -165,4 +165,12 @@ $mysql->close();
 EOPHP
 fi
 
-exec "$@"
+if [[ "$1" == nginx ]] || [ "$1" == php-fpm ]; 
+then
+  echo "Running PHP-FPM ..."
+  php-fpm --allow-to-run-as-root #--nodaemonize can stop run in foreground but we already have nginx in foreground so no issue with docker.
+  echo "Running Nginx ..."
+  nginx -g 'daemon off;'
+else
+ exec "$@"
+fi
